c2523896b8594febe7fd49dea63f268f4d1f28db
Javier Canizalez <javier-canizalez@outlook.com>
2024-05-22 18:03:45 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,24 @@
-provider "aws" {
-  region = "us-west-2"
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI ID
-  instance_type = "t2.micro"
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
 
   tags = {
-    Name = "example-instance"
+    Name = "HelloWorld"
   }
 }
